8d4daa87986f439ba9af339aab04dd63ba56044e
Adheesh Wadkar <apwadkar@asu.edu>
2020-05-22 01:26:30 -0700

    Add discord connection info

--- bot.py
+++ bot.py
@@ -4,10 +4,23 @@ import discord
 # Settings
 import settings
 
+# Connect to Redis backend cache
 r = redis.Redis(
     host=settings.REDIS_HOST,
     port=settings.REDIS_PORT,
     db=settings.REDIS_DB
 )
 
-print(r.keys('*'))
+class DiscordClient(discord.Client):
+    async def on_ready(self):
+        # Confirm that we logged into the bot user successfully
+        print(f'Logged on as {self}!')
+    
+    async def on_message(self, message):
+        # Ignore any messages from self
+        if message.author != self.user:
+            print('Message from {0.author}: {0.content}'.format(message))
+
+# Connect to Discord bot user
+disc = DiscordClient()
+disc.run(settings.DISCORD_TOKEN)
--- settings.py
+++ settings.py
@@ -3,6 +3,11 @@ import os
 
 load_dotenv()
 
+# Redis connection info
 REDIS_HOST=os.getenv('REDIS_HOST') or 'localhost'
 REDIS_PORT=int(os.getenv('REDIS_PORT') or '6379')
 REDIS_DB=int(os.getenv('REDIS_DB') or '0')
+
+# Discord info
+DISCORD_TOKEN=os.getenv('DISCORD_TOKEN') or ''
+OWNER_ID=os.getenv('OWNER_ID') or ''
