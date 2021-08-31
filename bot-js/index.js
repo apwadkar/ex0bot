877df20642675ccb6760dddcd42e363030ba56ba
Adheesh Wadkar <apwadkar@asu.edu>
2021-08-30 20:50:08 -0700

    Added events and command handling

--- bot-js/index.js
+++ bot-js/index.js
@@ -1,22 +1,26 @@
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
 const { TOKEN } = require('./config');
 
-const { Client, Intents } = require('discord.js');
 const client = new Client({
   intents: [Intents.FLAGS.GUILD_MEMBERS],
 });
 
-client.once('ready', () => {
-  console.log('Ready!');
-});
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
 
-client.on('interactionCreate', async (interaction) => {
-  if (!interaction.isCommand()) return;
-
-  const { commandName } = interaction;
-
-  if (commandName === 'ping') {
-    await interaction.reply('Pong');
+const eventFiles = fs.readdirSync('./events').filter((file) => file.endsWith('.js'));
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
   }
-});
+}
 
 client.login(TOKEN);
--- bot-js/register.js
+++ bot-js/register.js
@@ -1,10 +1,14 @@
-const { TOKEN, CLIENT_ID, GUILD_ID } = require('../config');
-
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
+const { TOKEN, CLIENT_ID, GUILD_ID } = require('./config');
 
-const commands = [new SlashCommandBuilder().setName('ping').setDescription('Replies with pong')].map((command) => command.toJSON());
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
 
 const rest = new REST({ version: '9' }).setToken(TOKEN);
 
