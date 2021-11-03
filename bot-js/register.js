0082182f26e518947533b2e658489c710df7d097
Adheesh Wadkar <apwadkar@asu.edu>
2021-11-03 00:13:34 -0700

    Added better dockerization

--- bot-js/register.js
+++ bot-js/register.js
@@ -4,7 +4,9 @@ const { Routes } = require('discord-api-types/v9');
 const { TOKEN, CLIENT_ID, GUILD_ID } = require('./config');
 
 const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter((file) => file.endsWith('.js'));
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   commands.push(command.data.toJSON());
@@ -14,9 +16,17 @@ const rest = new REST({ version: '9' }).setToken(TOKEN);
 
 (async () => {
   try {
-    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands });
-
-    console.log('Successfully registered application commands.');
+    if (process.env['DEVELOPMENT']) {
+      await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+        body: commands,
+      });
+      console.log(
+        `Successfully registered application guild commands to guild ${GUILD_ID}.`
+      );
+    } else {
+      await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+      console.log('Successfully registered application commands.');
+    }
   } catch (error) {
     console.error(error);
   }
